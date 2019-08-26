e9ede382477ac90949cdf30e8df3206a60e3f316
christine frank, Jacqueline Remmel and joel stockamp <joel@>
2019-08-26 10:06:58 -0700

    Add backend JS to create travel log and place objects. Add methods for adding place, removing place, getting place, updating memory, setting favorite.

---
+++ js/scripts.js
@@ -0,0 +1,62 @@
+// User Interface
+
+$(document).ready(function(){
+
+});
+
+
+// Business Logic
+function TravelLog(){
+  this.log = [],
+  this.currentId = 0
+}
+
+TravelLog.prototype.addPlace = function(name, memory = ""){
+  if (name) {
+    var place  = new Place(name, memory);
+    place.id = this.assignId();
+    this.log.push(place);
+  }
+}
+
+TravelLog.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+TravelLog.prototype.getPlace = function(id) {
+  for (var i = 0; i < this.log.length; i++) {
+    if (this.log[i]) {
+      if (this.log[i].id === id) {
+        return this.log[i];
+      }
+    }
+  }
+  return false;
+}
+
+TravelLog.prototype.removePlace = function(id) {
+  for (var i = 0; i < this.log.length; i++) {
+    if (this.log[i]) {
+      if (this.log[i].id === id) {
+        delete this.log[i];
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function Place(name, memory = ""){
+  this.name = name,
+  this.memory = memory,
+  this.isFavorite = false
+}
+
+Place.prototype.updateMemory = function(memory){
+  this.memory = memory;
+}
+
+Place.prototype.setFavorite = function(boolean){
+  this.isFavorite = boolean;
+}
